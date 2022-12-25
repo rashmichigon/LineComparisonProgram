110bd9a986f7642f1169ec540b4cd126ed6ca582
Rashmi <rashmichigon65@gmail.com>
2022-12-25 10:27:42 +0530

    UC3-CompareToMethod

--- Program.cs
+++ Program.cs
@@ -19,11 +19,21 @@
                 y2 = Convert.ToInt32(Console.ReadLine());
 
                 length[i] = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-                Console.WriteLine("length of the line is" + length);
+                Console.WriteLine("length of {0} ", i + length[i]);
             }
 
-            bool result = length[1].Equals(length[0]);
-            Console.WriteLine("Both lines are equals" + result);
+            if (length[0].Equals(length[1]))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else if (length[0] > length[1])
+            {
+                Console.WriteLine("Line 01 is greater than line 02");
+            }
+            else
+            {
+                Console.WriteLine("Line 02 is greater than line 01");
+            }
         }
 
     }
